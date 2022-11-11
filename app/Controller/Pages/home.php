970de88e3cbe0bddb16a16801ece2106cb23296a
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-11-11 15:40:54 -0300

    testando novo push

--- app/Controller/Pages/home.php
+++ app/Controller/Pages/home.php
@@ -5,6 +5,6 @@ namespace App\Controller\Pages;
 class Home{
     #método responsável por retornar o conteúdo (view) da página home
     public static function getHome(){
-
+        return "Olá Mundo!";
     }
 }
