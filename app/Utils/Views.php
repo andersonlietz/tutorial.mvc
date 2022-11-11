2bde8bddab86a4b3ed49b29865b7d3dd4a8ff49c
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-11-11 16:33:22 -0300

    testando novo commit

---
+++ app/Utils/Views.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace \App\Utils;
+
+class View{
+    
+    private static function(){
+        
+    }
+
+
+    /** 
+    * #método responsável por retornar o conteúdo de uma view
+
+    * @param  string $view
+    * @return string
+
+    */
+    public static render($view){
+
+
+    }
+}
+
+
+
+
+
+
